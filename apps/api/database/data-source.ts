e8501d74a846495da037440c0a681e657e2b635f
P.Pannawit <p.pannawit@192.168.0.105>
2026-04-25 20:48:14 +0700

    feat: setup migration generate script

--- apps/api/database/data-source.ts
+++ apps/api/database/data-source.ts
@@ -1,14 +1,13 @@
-import { config } from 'dotenv';
-import 'reflect-metadata';
-import { DataSource } from 'typeorm';
-import { CreateBook1710000000001 } from './migrations/1710000000001-CreateBook';
-import { InitPgvector1710000000000 } from './migrations/1710000000000-InitPgvector';
-import { databaseOptions } from './typeorm.options';
+import { config } from "dotenv";
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { databaseOptions } from "./typeorm.options";
 
-config({ path: '../../.env' });
+config({ path: "../../.env" });
 config();
 
 export default new DataSource({
   ...databaseOptions(),
-  migrations: [InitPgvector1710000000000, CreateBook1710000000001],
+  entities: [`${__dirname}/../src/**/*.entity{.ts,.js}`],
+  migrations: [`${__dirname}/migrations/*{.ts,.js}`],
 });
